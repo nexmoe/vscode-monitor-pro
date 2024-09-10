8b338efc24c212bdd0e82b05fa6d6ad7de30de3f
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-09-10 21:56:25 +0800

    chore: docs

---
+++ docs/app/source.ts
@@ -0,0 +1,8 @@
+import { docs, meta } from '@/.source';
+import { createMDXSource } from 'fumadocs-mdx';
+import { loader } from 'fumadocs-core/source';
+
+export const { getPage, getPages, pageTree } = loader({
+  baseUrl: '/docs',
+  source: createMDXSource(docs, meta),
+});
