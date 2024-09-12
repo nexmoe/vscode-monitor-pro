4e21ceb6c6bc30af9dbaa1e495bc90b84b23f6a1
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-09-12 21:22:10 +0800

    docs: 更新CTA按钮并配置Tailwind动画
    - 在docs/app/page.tsx中，将CTA按钮组件从Button更改为ShimmerButton，以增强视觉效果。
    - 调整了英雄部分的布局，并应用了ShimmerButton以保持设计一致性。
    - 在tailwind.config.ts中，新增了自定义动画，包括“spin-around”和“shine-pulse”，以支持新的UI元素。
    - 尾风配置的整体主题调整中省略了暗模式设置，以保持一致性。

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 const config = {
+  darkMode: ["class"],
   content: [
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
@@ -9,7 +10,6 @@ const config = {
     './mdx-components.{ts,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
   ],
-  darkMode: ["class"],
   presets: [createPreset()],
   prefix: "",
   theme: {
@@ -70,10 +70,42 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
       },
     },
   },
